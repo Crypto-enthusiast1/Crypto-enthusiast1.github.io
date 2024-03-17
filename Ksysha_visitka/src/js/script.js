64c154364347690cc474d5140640fa30c0db133a
Artem <andrei28123@gmail.com>
2024-03-17 23:10:06 +0100

    JS ready

---
+++ Ksysha_visitka/src/js/script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+   const images = ['url(src/img/photo1.JPG)', 'url(src/img/photo2.JPG)', 'url(src/img/photo3.JPG)', 'url(src/img/photo4.JPG)'];
+   const descr = document.querySelector('.description');
+   let index = 0;
+   function changeImg() {
+      document.body.style.background = images[index] + "center center/cover no-repeat";
+      index = (index + 1) % images.length;
+      if (index > 1) {
+         descr.innerHTML = `Representative of the Dnipropetrovsk Association of Local Governments and the Dnipropetrovsk Region in the Alliance of the EU Committee of the Regions. </br> </br>
+         + 32 470 50 42 33 </br>
+         dniprorada.bruxelles@gmail.com`
+      } else {
+         descr.innerHTML = `
+         Head of business hub </br>
+         +32 470 50 42 33</br>
+         dir.bsnshub.bruxelles@gmail.com </br></br>
+
+         Your representative in Europe, who is no less interested in your success than you are!`
+      }
+   }
+
+   changeImg();
+
+   setInterval(changeImg, 3000);
+});
